94aa77e7f1345c6a47b4f6ac1f8be3ae6a83adc9
Gene Black <geblack@hotmail.com>
2017-11-27 13:58:19 -0600

    encrypt/decrypt node ID

---
+++ lib/graphql_model_mapper/encryption.rb
@@ -0,0 +1,20 @@
+module GraphqlModelMapper
+  module Encryption
+    def self.key
+      Digest::SHA256.digest(ENV['GRAPHQL_SECRET_TOKEN'] || ENV['SECRET_TOKEN'] || GraphqlModelMapper.secret_token)
+    end
+
+    def self.aes(m,t)
+      (aes = OpenSSL::Cipher::Cipher.new('aes-256-cbc').send(m)).key = Digest::SHA256.digest(self.key)
+      aes.update(t) << aes.final
+    end
+    
+    def self.encode(text)
+      Base64.encode64(aes(:encrypt, text)).strip
+    end
+    
+    def self.decode(text)
+      aes(:decrypt, Base64.decode64(text))
+    end
+  end
+end
